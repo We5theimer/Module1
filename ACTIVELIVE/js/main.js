681c31ed3f61c16a1fe3958d3989e142f97ac56b
We5theimer <132555863+We5theimer@users.noreply.github.com>
2023-10-04 15:43:31 +0800

    Delete ACTIVELIVE directory

--- ACTIVELIVE/js/main.js
+++
@@ -1,9 +0,0 @@
-$(".slow").click(function (event) {
-  event.preventDefault();
-  var elementClick = $(this).attr("href");
-  var destination = $(elementClick).offset().top;
-  $("html:not(:animated),body:not(:animated)").animate(
-    { scrollTop: destination },
-    800
-  );
-});
